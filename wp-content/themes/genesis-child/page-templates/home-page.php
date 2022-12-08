b6ea07116fdff39a84d945936dc61b7e9cc24293
saima <saima.rana@xotiv.com>
2022-12-08 12:01:28 +0530

    service section

--- wp-content/themes/genesis-child/page-templates/home-page.php
+++ wp-content/themes/genesis-child/page-templates/home-page.php
@@ -48,19 +48,19 @@
     <h3 class="">Software development, IT outsourcing...</h3>
           <h3 class="mb-4">Why not go beyond?</h3>
           <p class="lead">We work with specific purposes and intended result in mind. </p>
-          <a class="mt-5 btn btn-blue " href="https://www.xotiv.com/about-us/"><span>More</span></a>
+          <a class="mt-5 btn btn-primary" href="https://www.xotiv.com/about-us/"><span>More</span></a>
     </div>
     <div class="item">
     <h3 class="">Mobile Application development</h3>
           <h3 class="mb-4">Why not go beyond?</h3>
           <p class="lead">Get Top-Notch, Extensively, Innovative Mobile App Solution BY Us.</p> 
-          <a class="mt-5 btn btn-blue" href="https://www.xotiv.com/about-us/"><span>More</span></a>
+          <a class="mt-5 btn btn-primary" href="https://www.xotiv.com/about-us/"><span>More</span></a>
     </div>
     <div class="item">
       <h3 class="">Mobile Application development</h3>
       <h3 class="mb-4">Why not go beyond?</h3>
       <p class="lead">We work with specific purposes and intended result in mind. </p>
-      <a class="mt-5 btn btn-blue " href="https://www.xotiv.com/about-us/"><span>More</span></a>
+      <a class="mt-5 btn btn-primary " href="https://www.xotiv.com/about-us/"><span>More</span></a>
     </div>
     <div class="item">
       <h3 class="">Mobile Application development</h3>
@@ -84,22 +84,76 @@
         </div>
       </div>
       <div class="col-lg-7">
-      <div class="section-title">
-        <div class="title"> 
+      <div class="about-section-2">
           <div><img class="img-fluid" src="http://localhost/Xotiv-Technology/wp-content/uploads/2022/12/about-icon.png" alt=""> ABOUT US</div>
-        </div>
-        <div class="about-front">Leading Digital Agency For Business Solution</div>
+        <div class="heading-subtitle">Leading Digital Agency For Business Solution</div>
          <p class="pt-5">Xotiv Technologies offers a wide range of service that includes Artificial Intelligence, Machine Learning, Mobile Application, Web App Development, and other Application Development and Maintenance including a wide range of digital marketing service for digital transformation
         </p>
-        <a class="btn btn-primary mt-4" href="https://www.xotiv.com/about-us/"><span>Learn More</span></a>
+        <a class="btn btn-secondary mt-4" href="https://www.xotiv.com/about-us/"><span>Learn More</span></a>
       </div>
     </div>
    </div>
 </section>
 
 
+<section class="container-fluid service-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="row">
+          <div class="col-lg-6 d-flex align-self-center">
+            <div class="service">
+              <div class="service-icon">
+              <div><img class="img-fluid" src="http://localhost/Xotiv-Technology/wp-content/uploads/2022/12/s1.png" alt=""> Artificial Intelligence</div>
+              </div>
+              <div class="service-desc">
+                <p class="pt-4">AI is a leading-edge technology that helps machines to act independently, resulting in the better implementation of iterative tasks.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+          <div class="service">
+              <div class="service-icon">
+              <div><img class="img-fluid" src="http://localhost/Xotiv-Technology/wp-content/uploads/2022/12/s2.png" alt=""> Web Application</div>
+              </div>
+              <div class="service-desc">
+                <p class="pt-4">Xotiv using latest high ended technologies and customized tools for developing industry-specific Website and webApps to engage with customers to automate processes</p>
+              </div>
+            </div>
+            <div class="service mt-5">
+              <div class="service-icon">
+              <div><img class="img-fluid" src="http://localhost/Xotiv-Technology/wp-content/uploads/2022/12/s3.png" alt=""> Mobile App Development</div>
+              </div>
+              <div class="service-desc">
+                <p class="pt-4">Top-Notch Mobile App Development Services all over the World. Mobile application management is an administration and delivery of enterprise software. Xotiv Technology comes with the best mobile application management system</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 d-flex align-self-center flex-column">
+      <div class="heading-title"><img class="img-fluid" src="http://localhost/Xotiv-Technology/wp-content/uploads/2022/12/service-icon.png" alt=""> OUR SERVICE</div>
+      <div class="heading-subtitle">We'v Been Providing Our Services</div>
+      <p class="pt-5">Xotiv Technology offers complete customized software development services across diverse domains and verticals. We offer a wide array of IT and IT-enabled services based on the requirement of our client. We use structured methodologies with proven IT processes that help abundantly to reduce the risks or complexity</p>
+      <a class="btn btn-service mt-4" href="https://www.xotiv.com/services/"><span>Explore More</span></a>
+    </div>
+    </div>
+  </div>
+</section>
 
 
+
+
+<section class="container-fluid modern-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 text-center">
+        <div class="heading-subtitle">We Build Software For Modern Platforms</div>
+        <p class="px-5">Xotiv Technologies is the premier service in complete Development and Quality Assurance and specialized in the innovative development of the time-sensitive service and on-premises solutions.</p>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php  get_footer(); ?>
 
